fa082d01f6eaf911d3bfe9402c9f6bd719d3d143
Jeremy Lincoln <jeremy.lincoln@bisonline.com>
2021-11-19 12:10:21 -0500

    Finished Section 3

---
+++ Section3/main.rb
@@ -0,0 +1,10 @@
+#-- Arithmetic +, -, *, /, **
+# PEMDAS
+puts 5 + 15 * 20 - 2 / 6**3 - (3+1)
+
+#-- Int (1,2,3) vs Float(1.2, 3.43) vs Decimal(1.2323232323, 45.332567534265)
+# int
+puts 2/216
+# float or decimal
+puts 2.0/216
+
